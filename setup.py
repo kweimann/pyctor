4ee56d69ffd929f9a659e095aa9742d9939c2ed6
kweimann <kuba.weimann@gmail.com>
2020-11-17 14:54:54 +0100

    Update setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
     name='pyctor',
     packages=['pyctor'],
     version='0.0.1',
     license='MIT',
     description='Minimalistic implementation of an actor concurrency model powered by asyncio.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Kuba Weimann',
     author_email='kuba.weimann@gmail.com',
     url='https://github.com/kweimann/pyctor',
+    python_requires='>=3.7.3',
 )
